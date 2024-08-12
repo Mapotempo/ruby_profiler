942e9ece3871106cf3f94c2c3506563d20f15697
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2024-08-12 14:30:53 +0000

    simplify usage init

--- lib/rake-profiler/rails/railtie.rb
+++ lib/rake-profiler/rails/railtie.rb
@@ -1,11 +1,9 @@
 module RakeProfiler::Rails
-  class Railtie < Rails::Railtie
-    def self.initialize!(app)
-      raise "RakeProfilerRails initialized twice. Set `require: false' for rack-profiler in your Gemfile" if defined?(@already_initialized) && @already_initialized
+  def self.initialize!(app)
+    raise "RakeProfilerRails initialized twice. Set `require: false' for rack-profiler in your Gemfile" if defined?(@already_initialized) && @already_initialized
 
-      return unless Rails.env.development?
+    return unless Rails.env.development?
 
-      app.middleware.insert(0, RakeProfiler::Middleware)
-    end
+    app.middleware.insert(0, RakeProfiler::Middleware)
   end
 end
