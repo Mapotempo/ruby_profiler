0ed9f67731796e586ba2e69955683ff3a0ab26aa
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2024-08-12 14:27:23 +0000

    load file into rake-profiler

--- lib/rake-profiler.rb
+++ lib/rake-profiler.rb
@@ -1,6 +1,11 @@
 require "rake-profiler/version"
+require 'rake-profiler/middleware'
 
 module RakeProfiler
   class Error < StandardError; end
   # Your code goes here...
+end
+
+if defined?(::Rails) && defined?(::Rails::VERSION) && ::Rails::VERSION::MAJOR.to_i >= 3
+  require 'rake-profiler/rails/railtie'
 end
