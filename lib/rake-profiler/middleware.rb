6b8584d0c02827c075e2b1c4a86ff86c236c2553
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2024-08-12 16:54:45 +0200

    add time

--- lib/rake-profiler/middleware.rb
+++ lib/rake-profiler/middleware.rb
@@ -11,6 +11,8 @@ module RakeProfiler
     end
 
     def call(env)
+      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      request = ::Rack::Request.new(env)
       StackProf.start(
         mode: :wall,
         interval: 1000,
@@ -21,22 +23,36 @@ module RakeProfiler
       @app.call(env)
     ensure
       StackProf.stop
-      save
+      end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      delta_time = ((end_time - start_time) * 1000).to_i
+      save(request, delta_time)
     end
 
     private
 
-    def save
+    def save(request, delta_time)
       results = StackProf.results
       return if results.blank?
 
-      filename = "profiling-#{results[:mode]}-#{Process.pid}-#{Time.now.to_i}.json"
+      route = rails_route_from_path(request.path, request.request_method)
+      filename = "#{Time.now.to_i}-profiling-#{results[:mode]}-#{Process.pid}-#{route}-#{delta_time}ms.json"
+      filename = sanitize_filename(filename)
       path = 'tmp/profiling'
 
       FileUtils.mkdir_p(path)
       File.open(File.join(path, filename), 'wb') do |f|
         f.write JSON.generate(results)
       end
+    end
+
+    def rails_route_from_path(path, method)
+      hash = Rails.application.routes.recognize_path(path, method: method)
+      return unless hash && hash[:controller] && hash[:action]
+
+      Rails.logger.info hash
+      "#{hash[:controller]}##{hash[:action]}"
+    rescue ActionController::RoutingError
+      nil
     end
   end
 end
