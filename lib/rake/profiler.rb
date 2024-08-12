791b7799394b28f21da5c433a1353637410f6d13
Giallombardo Nathan <nathan.giallombardo@melting-point.fr>
2024-08-12 13:31:03 +0000

    init gems

---
+++ lib/rake/profiler.rb
@@ -0,0 +1,8 @@
+require "rake/profiler/version"
+
+module Rake
+  module Profiler
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rake/profiler/version.rb
@@ -0,0 +1,5 @@
+module Rake
+  module Profiler
+    VERSION = "0.1.0"
+  end
+end
